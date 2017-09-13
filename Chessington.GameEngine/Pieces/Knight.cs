eada56ad53548fe8ec5b7a4a2b9f1358b2771517
Josh Powell <Josh.Powell@softwire.com>
2017-09-13 15:25:48 +0100

    Knight moves appropriately

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var knightLocation = board.FindPiece(this);
+            var possibleMovements = new List<Square>();
+            for (int i = -2; i < 3; i = i + 4)
+            {
+                possibleMovements.Add(new Square(knightLocation.Row + i, knightLocation.Col + 1));
+                possibleMovements.Add(new Square(knightLocation.Row + i, knightLocation.Col - 1));
+            }
+            for (int i = -1; i <2 ; i = i + 2)
+            {
+                possibleMovements.Add(new Square(knightLocation.Row + i, knightLocation.Col + 2));
+                possibleMovements.Add(new Square(knightLocation.Row - i, knightLocation.Col - 2));
+            }
+
+
+            return possibleMovements;
         }
     }
 }
