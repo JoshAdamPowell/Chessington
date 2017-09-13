9c0d2f7497cfb553f36d6405d03b39fa4c491cb4
Josh Powell <Josh.Powell@softwire.com>
2017-09-13 16:28:06 +0100

    Rooks will not go past other pieces

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -12,13 +12,55 @@ namespace Chessington.GameEngine.Pieces
         {
             var rookLocation = board.FindPiece(this);
             var possibleLocations = new List<Square>();
+
+            for (var i = rookLocation.Row - 1; i >= 0; i--)
+            {
+                var potentialLocation = new Square(i, rookLocation.Col);
+                possibleLocations.Add(potentialLocation);
+                if (board.IsOccupied(potentialLocation))
+                {
+                    break;
+                }
+            }
+            for (var i = rookLocation.Row + 1; i < 8; i++)
+            {
+                var potentialLocation = new Square(i, rookLocation.Col);
+                possibleLocations.Add(potentialLocation);
+                if (board.IsOccupied(potentialLocation))
+                {
+                    break;
+                }
+            }
+            for (var i = rookLocation.Col - 1; i >= 0 ; i--)
+            {
+                var potentialLocation = new Square(rookLocation.Row, i);
+                possibleLocations.Add(potentialLocation);
+                if (board.IsOccupied(potentialLocation))
+                {
+                    break;
+                }
+            }
+            for (var i = rookLocation.Col + 1; i < 8; i++)
+            {
+                var potentialLocation = new Square(rookLocation.Row, i);
+                possibleLocations.Add(potentialLocation);
+                if (board.IsOccupied(potentialLocation))
+                {
+                    break;
+                }
+            }
+
+            
+
+
+
+            /*
             for (var i = 0; i <= 8; i++)
             {
                 possibleLocations.Add(new Square(rookLocation.Row, i));
                 possibleLocations.Add(new Square(i, rookLocation.Col));
             }
-
-
+            */
             return possibleLocations;
         }
     }
