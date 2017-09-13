7a60007ffc6dcbfbac378a034a557a3d82368114
Josh Powell <Josh.Powell@softwire.com>
2017-09-13 14:01:02 +0100

    Queen now moves apparopriately.

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,31 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var queenLocation = board.FindPiece(this);
+            var lateralMovements = new List<Square>();
+            for (var i = 0; i <= 8; i++)
+            {
+                lateralMovements.Add(new Square(queenLocation.Row, i));
+                lateralMovements.Add(new Square(i, queenLocation.Col));
+            }
+            var validLateralMovements = lateralMovements.Where(x => x.Row < 8 && x.Row >= 0).Where(x => x.Col < 8 && x.Row >= 0)
+                .Select(x => x).ToList();
+            var diagonalMovements = new List<Square>();
+            for (var i = 1; i <= 8; i++)
+            {
+                diagonalMovements.Add(new Square(queenLocation.Row + i, queenLocation.Col + i));
+                diagonalMovements.Add(new Square(queenLocation.Row + i, queenLocation.Col - i));
+                diagonalMovements.Add(new Square(queenLocation.Row - i, queenLocation.Col + i));
+                diagonalMovements.Add(new Square(queenLocation.Row - i, queenLocation.Col - i));
+
+            }
+            var validDiagnonalMovements = diagonalMovements.Where(x => x.Row < 8 && x.Row >= 0).Where(x => x.Col < 8 && x.Row >= 0)
+                .Select(x => x).ToList();
+
+            var possibleMovements = validLateralMovements.Union(validDiagnonalMovements).ToList();
+            possibleMovements.Remove(queenLocation);
+
+            return possibleMovements;
         }
     }
 }
