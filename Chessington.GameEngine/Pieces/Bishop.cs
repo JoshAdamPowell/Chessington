192cbbd486c436cfc60bc0dca3bd7a453bac3cfe
Josh Powell <Josh.Powell@softwire.com>
2017-09-13 13:46:25 +0100

    Bishop now works appropriately.

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,12 +11,22 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            
+            var bishopLocation = board.FindPiece(this);
+            var possibleLocations = new List<Square>();
+
+            for (var i = 1; i <= 8; i++)
+            {
+                possibleLocations.Add(new Square(bishopLocation.Row + i, bishopLocation.Col + i));
+                possibleLocations.Add(new Square(bishopLocation.Row + i, bishopLocation.Col - i));
+                possibleLocations.Add(new Square(bishopLocation.Row - i, bishopLocation.Col + i));
+                possibleLocations.Add(new Square(bishopLocation.Row - i, bishopLocation.Col - i));
+
+            }
+            var newResult = possibleLocations.Where(x => x.Row < 8 && x.Row >= 0).Where(x => x.Col < 8 && x.Row >= 0)
+                .Select(x => x).ToList();
 
 
-
-
-            return Enumerable.Empty<Square>();
+            return newResult;
         }
     }
 }
